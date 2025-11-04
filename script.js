2982511fe960e687b3a447547c0817e9255889c2
dwivediyeeshu <yeeshuwork@gmail.com>
2025-11-04 14:55:14 +0530

    Added the script.js and added the dom elements

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+});
